243d4eb7da08a10e3fb294184a9fe4527f67da3e
Afshin Razaghi <neo1.afshin@gmail.com>
2023-05-26 14:58:45 +0330

    Convert Web Project From Razor Pages To Model-View-Controller

--- MarketPlace.Web/Program.cs
+++ MarketPlace.Web/Program.cs
@@ -12,7 +12,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -24,6 +24,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
