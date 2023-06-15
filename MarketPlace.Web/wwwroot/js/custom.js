e51c55d6e68e3357bf961ce4392f5c7a9a08b525
Afshin Razaghi <neo1.afshin@gmail.com>
2023-06-15 19:13:54 +0330

    add ckeditor as editor to ticket pages

--- MarketPlace.Web/wwwroot/js/custom.js
+++ MarketPlace.Web/wwwroot/js/custom.js
@@ -4,9 +4,64 @@
         displayCloseButton: false,
         positionClass: 'nfc-bottom-right',
         showDuration: 4000,
-        theme : theme !== '' ? theme : 'success'
+        theme: theme !== '' ? theme : 'success'
     })({
         title: title !== '' ? title : 'اعلان',
         message: decodeURI(message)
     });
 }
+
+
+$(document).ready(function () {
+    debugger;
+    let editors = $("[ckeditor]");
+    if (editors.length > 0) {
+        $.getScript('/js/ckeditor.js', function () {
+            editors.each(function (index, item) {
+                let id = $(item).attr("ckeditor");
+                ClassicEditor.create(document.querySelector('[ckeditor="' + id + '"]'),
+                    {
+                        toolbar: {
+                            items: [
+                                'heading',
+                                '|',
+                                'bold',
+                                'italic',
+                                'link',
+                                '|',
+                                'fontSize',
+                                'fontColor',
+                                '|',
+                                'imageUpload',
+                                'blockQuote',
+                                'insertTable',
+                                'undo',
+                                'redo',
+                                'codeBlock'
+                            ]
+                        },
+                        language: 'fa',
+                        table: {
+                            contentToolbar: [
+                                'tableColumn',
+                                'tableRow',
+                                'mergeTableCells'
+                            ]
+                        },
+                        licenseKey: '',
+                        simpleUpload: {
+                            // The URL that the images are uploaded to.
+                            uploadUrl: '/Uploader/UploadImage'
+                        }
+
+                    })
+                    .then(editor => {
+                        window.editor = editor;
+                    }).catch(err => {
+                        console.error(err);
+                    });
+            });
+        });
+
+    }
+});
