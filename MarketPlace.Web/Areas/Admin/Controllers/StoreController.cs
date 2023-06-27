5fb27c2c68569dd9e1ca6454b04cde80ccc1533e
Afshin Razaghi <neo1.afshin@gmail.com>
2023-06-27 08:49:38 +0330

    add store management menu and page to admin panel

---
+++ MarketPlace.Web/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using MarketPlace.Application.Services.Interfaces;
+using MarketPlace.DataLayer.DTOs.Stores;
+using MarketPlace.Web.PresentationExtensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Areas.Admin.Controllers
+{
+    public class StoreController : AdminBaseController
+    {
+        #region constructor
+        private readonly IStoreService _storeService;
+        public StoreController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+        #endregion
+
+        #region store-requests
+        [HttpGet("store-requests")]
+        public async Task<IActionResult> StoreRequests(FilterStoreDTO filter)
+        {
+            filter.UserId = User.GetUserId()!.Value;
+            return View(await _storeService.FilterStore(filter));
+        }
+        #endregion    
+    }
+}
