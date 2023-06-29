9aa3f9c0317cfa2d2080631050528b3162f32781
Afshin Razaghi <neo1.afshin@gmail.com>
2023-06-29 17:53:54 +0330

    Add Store Area

---
+++ MarketPlace.Web/Areas/Store/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Areas.Store.Controllers
+{
+    public class HomeController : StoreBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MarketPlace.Web/Areas/Store/Controllers/StoreBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Areas.Store.Controllers
+{
+    [Authorize]
+    [Area("Store")]
+    public class StoreBaseController : Controller
+    {
+       
+    }
+}
